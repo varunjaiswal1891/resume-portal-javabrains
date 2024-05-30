c49f02bdd9525ec194d54691c47a8d7a7f507a0a
Varun Jaiswal <varjaisw@cisco.com>
2024-05-30 22:10:50 +0530

    code reset done 30 may

--- resume-portal/src/main/java/com/javabrains/resume_portal/ResumePortalApplication.java
+++ resume-portal/src/main/java/com/javabrains/resume_portal/ResumePortalApplication.java
@@ -1,9 +1,11 @@
-package io.javabrains.resume_portal;
+package com.javabrains.resume_portal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackageClasses = UserRepository.class)
 public class ResumePortalApplication {
 
 	public static void main(String[] args) {
---
+++ resume-portal/src/main/java/com/javabrains/resume_portal/UserRepository.java
@@ -0,0 +1,9 @@
+package com.javabrains.resume_portal;
+
+import com.javabrains.resume_portal.models.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUserName(String userName);
+}
